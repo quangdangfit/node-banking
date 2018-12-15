7d5bb3b11e9d7c0c4a8f34d6bb40b23e7fc3c044
Quang Dang <quang.dp@teko.vn>
2018-12-15 13:17:07 +0700

    add sql file (included users_account)

--- sql/internet_banking.sql
+++ sql/internet_banking.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50724
  File Encoding         : 65001
 
- Date: 15/12/2018 08:56:39
+ Date: 15/12/2018 13:16:37
 */
 
 SET NAMES utf8mb4;
@@ -39,6 +39,28 @@ INSERT INTO `users` VALUES (1, 'admin', '81dc9bdb52d04dc20036dbd8313ed055', 'Qua
 INSERT INTO `users` VALUES (7, 'quang', '81dc9bdb52d04dc20036dbd8313ed055', 'Quang', 'Dang', 'quang@gmail.com', 789456123);
 
 -- ----------------------------
+-- Table structure for users_account
+-- ----------------------------
+DROP TABLE IF EXISTS `users_account`;
+CREATE TABLE `users_account`  (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `uid` int(10) UNSIGNED NOT NULL,
+  `account_number` int(16) UNSIGNED NOT NULL,
+  `balance` int(16) UNSIGNED NULL DEFAULT 0,
+  `create_at` datetime(6) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`, `account_number`) USING BTREE,
+  INDEX `user_account_frk`(`uid`) USING BTREE,
+  CONSTRAINT `user_account_frk` FOREIGN KEY (`uid`) REFERENCES `users` (`uid`) ON DELETE CASCADE ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 31 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of users_account
+-- ----------------------------
+INSERT INTO `users_account` VALUES (28, 1, 1997000000, 0, '2018-12-15 13:14:05.000000');
+INSERT INTO `users_account` VALUES (29, 1, 1997000001, 0, '2018-12-15 13:14:13.000000');
+INSERT INTO `users_account` VALUES (30, 1, 1997000002, 0, '2018-12-15 13:14:18.000000');
+
+-- ----------------------------
 -- Table structure for users_token
 -- ----------------------------
 DROP TABLE IF EXISTS `users_token`;
@@ -56,6 +78,6 @@ CREATE TABLE `users_token`  (
 -- ----------------------------
 -- Records of users_token
 -- ----------------------------
-INSERT INTO `users_token` VALUES (1, 1, 'yugTdkyadLHqTe7mfWcS9iyU1oCmecSlmVX2vDFMAmfcjG1DknDqNJkUJv7T5i6rIEWXG4FaOi9MCbvJ', '2018-12-15 08:50:50.000000', '2018-12-15 08:52:38.000000');
+INSERT INTO `users_token` VALUES (1, 1, 'zFv3XZjcgCLpQjod4kMU53N2PWEbMZe1pbPDgZqRzsTr4vhvaydD4h3mjWRO8SS0fQ9hSQpDy9XHlpZq', '2018-12-15 08:50:50.000000', '2018-12-15 13:13:09.000000');
 
 SET FOREIGN_KEY_CHECKS = 1;
