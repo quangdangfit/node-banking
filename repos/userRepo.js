0ccc5748cb620d8e929063c88e276a602fed9713
Quang Dang <quang.dp@teko.vn>
2018-12-15 08:55:21 +0700

    add authRepo to authenticate by token

--- repos/userRepo.js
+++ repos/userRepo.js
@@ -1,16 +1,16 @@
 var md5 = require('crypto-js/md5'),
   kn = require('../fn/db');
 
-exports.list = () => kn('users').select('username', 'first_name', 'last_name', 'email', 'phone');
+exports.list = () => kn('users').select('uid', 'username', 'first_name', 'last_name', 'email', 'phone');
 
 exports.single = id => kn('users')
-  .select('username', 'first_name', 'last_name', 'email', 'phone')
+  .select('uid', 'username', 'first_name', 'last_name', 'email', 'phone')
   .where('uid', id)
   .first();
 
 exports.add = input => {
   input.password = md5(input.password).toString();
-  return kn('users').insert(input).returning('uid').returning('uid')
+  return kn('users').insert(input).returning('uid')
 };
 
 exports.delete = id => kn('users')
@@ -23,10 +23,10 @@ exports.update = (id, input) => kn('users')
 
 exports.login = (input) => {
   var md5_pwd = md5(input.password).toString();
-  return kn('users').select('uid').where({
+  return kn('users').select('uid', 'username', 'first_name', 'last_name', 'email', 'phone').where({
     'username': input.username,
     'password': md5_pwd
-  })
+  }).first()
 };
 
 
