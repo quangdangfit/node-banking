19c9eab1a2f88e1aeb4b72c5e1f0952cf59708f9
QuangPhuocDang <quang.dp@teko.vn>
2018-12-23 12:17:33 +0700

    update database

--- sql/internet_banking.sql
+++ sql/internet_banking.sql
@@ -11,11 +11,65 @@
  Target Server Version : 50724
  File Encoding         : 65001
 
- Date: 15/12/2018 16:15:27
+ Date: 23/12/2018 12:16:36
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for fee_type
+-- ----------------------------
+DROP TABLE IF EXISTS `fee_type`;
+CREATE TABLE `fee_type`  (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of fee_type
+-- ----------------------------
+INSERT INTO `fee_type` VALUES (1, 'Recipients Paid');
+INSERT INTO `fee_type` VALUES (2, 'Sender Paid');
+
+-- ----------------------------
+-- Table structure for transfers
+-- ----------------------------
+DROP TABLE IF EXISTS `transfers`;
+CREATE TABLE `transfers`  (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `src_account` int(16) UNSIGNED NOT NULL,
+  `dest_account` int(16) UNSIGNED NOT NULL,
+  `amount` int(16) NOT NULL,
+  `fee_type` int(6) NOT NULL DEFAULT 1,
+  `note` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `date` datetime(6) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `transfer_account_src`(`src_account`) USING BTREE,
+  INDEX `transfer_account_dest`(`dest_account`) USING BTREE,
+  CONSTRAINT `transfer_account_dest` FOREIGN KEY (`dest_account`) REFERENCES `users_account` (`account_number`) ON DELETE CASCADE ON UPDATE RESTRICT,
+  CONSTRAINT `transfer_account_src` FOREIGN KEY (`src_account`) REFERENCES `users_account` (`account_number`) ON DELETE CASCADE ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 19 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of transfers
+-- ----------------------------
+INSERT INTO `transfers` VALUES (4, 1997000000, 1997000001, 100000, 1, NULL, '2018-12-16 14:19:27.000000');
+INSERT INTO `transfers` VALUES (5, 1997000000, 1997000001, 100000, 1, NULL, '2018-12-16 14:21:02.000000');
+INSERT INTO `transfers` VALUES (6, 1997000000, 1997000001, 1000000, 1, NULL, '2018-12-16 14:26:28.000000');
+INSERT INTO `transfers` VALUES (7, 1997000000, 1997000001, 10000000, 1, NULL, '2018-12-16 14:31:57.000000');
+INSERT INTO `transfers` VALUES (8, 1997000000, 1997000002, 1000000, 1, NULL, '2018-12-16 14:33:00.000000');
+INSERT INTO `transfers` VALUES (9, 1997000000, 1997000002, 1000000, 1, NULL, '2018-12-16 14:33:29.000000');
+INSERT INTO `transfers` VALUES (10, 1997000000, 1997000002, 1000000, 1, NULL, '2018-12-16 14:33:34.000000');
+INSERT INTO `transfers` VALUES (11, 1997000000, 1997000002, 1000000, 1, NULL, '2018-12-16 14:49:11.000000');
+INSERT INTO `transfers` VALUES (12, 1997000001, 1997000004, 1000000, 1, NULL, '2018-12-16 14:49:51.000000');
+INSERT INTO `transfers` VALUES (13, 1997000001, 1997000004, 1000000, 1, NULL, '2018-12-16 14:49:57.000000');
+INSERT INTO `transfers` VALUES (14, 1997000001, 1997000004, 1000000, 1, NULL, '2018-12-16 14:50:04.000000');
+INSERT INTO `transfers` VALUES (15, 1997000001, 1997000004, 1000000, 1, NULL, '2018-12-16 16:52:30.000000');
+INSERT INTO `transfers` VALUES (16, 1997000001, 1997000004, 1000000, 1, NULL, '2018-12-16 16:53:00.000000');
+INSERT INTO `transfers` VALUES (17, 1997000001, 1997000004, 1000000, 1, NULL, '2018-12-16 16:54:42.000000');
+INSERT INTO `transfers` VALUES (18, 1997000004, 1997000001, 1000000, 1, NULL, '2018-12-16 16:56:31.000000');
 
 -- ----------------------------
 -- Table structure for users
@@ -31,7 +85,7 @@ CREATE TABLE `users`  (
   `phone` decimal(20, 0) NULL DEFAULT NULL,
   `role` int(10) NULL DEFAULT 3,
   PRIMARY KEY (`uid`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 10 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of users
@@ -52,20 +106,19 @@ CREATE TABLE `users_account`  (
   `create_at` datetime(6) NULL DEFAULT NULL,
   PRIMARY KEY (`id`, `account_number`) USING BTREE,
   INDEX `user_account_frk`(`uid`) USING BTREE,
+  INDEX `id`(`id`) USING BTREE,
+  INDEX `account_number`(`account_number`) USING BTREE,
   CONSTRAINT `user_account_frk` FOREIGN KEY (`uid`) REFERENCES `users` (`uid`) ON DELETE CASCADE ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 36 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 33 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of users_account
 -- ----------------------------
-INSERT INTO `users_account` VALUES (28, 1, 1997000000, 0, '2018-12-15 13:14:05.000000');
-INSERT INTO `users_account` VALUES (29, 1, 1997000001, 0, '2018-12-15 13:14:13.000000');
-INSERT INTO `users_account` VALUES (30, 1, 1997000002, 0, '2018-12-15 13:14:18.000000');
+INSERT INTO `users_account` VALUES (28, 1, 1997000000, 4720000, '2018-12-15 13:14:05.000000');
+INSERT INTO `users_account` VALUES (29, 1, 1997000001, 1190000, '2018-12-15 13:14:13.000000');
+INSERT INTO `users_account` VALUES (30, 1, 1997000002, 4000000, '2018-12-15 13:14:18.000000');
 INSERT INTO `users_account` VALUES (31, 1, 1997000003, 0, '2018-12-15 14:23:07.000000');
-INSERT INTO `users_account` VALUES (32, 1, 1997000004, 0, '2018-12-15 14:37:37.000000');
-INSERT INTO `users_account` VALUES (33, 1, 1997000005, 0, '2018-12-15 14:40:22.000000');
-INSERT INTO `users_account` VALUES (34, 1, 1997000006, 0, '2018-12-15 14:40:32.000000');
-INSERT INTO `users_account` VALUES (35, 1, 1997000007, 0, '2018-12-15 15:24:21.000000');
+INSERT INTO `users_account` VALUES (32, 1, 1997000004, 8980000, '2018-12-15 14:37:37.000000');
 
 -- ----------------------------
 -- Table structure for users_role
@@ -102,7 +155,7 @@ CREATE TABLE `users_token`  (
 -- ----------------------------
 -- Records of users_token
 -- ----------------------------
-INSERT INTO `users_token` VALUES (1, 1, 'tyIKqMSv2EglLnoUnWMdRKKLQH46m2fYMy5CiOPd0vGTWaM0Uwsp2WQWBxWlYk8O5bRx61DIFceLCAhC', '2018-12-15 08:50:50.000000', '2018-12-15 16:12:35.000000');
-INSERT INTO `users_token` VALUES (2, 7, 'Mu6RfPwNrWLouSWeXAX268KzgoPRqK6UBiATCt2sk1mP8HEhtZgiahJL3tfqUcKpOg74FVn7Nt1Ba3t8', '2018-12-15 16:13:18.000000', NULL);
+INSERT INTO `users_token` VALUES (1, 1, 'HODLbAh2SlukcYSd0oiRhPAoz6BlJqVVD2JESi8IH5mF9esg3B6KSj2mTaNJXi7mYqNyYq2Y05ZixQzg', '2018-12-15 08:50:50.000000', '2018-12-16 16:45:10.000000');
+INSERT INTO `users_token` VALUES (2, 7, 'uFzaxvgTpPkB4r8AJ7JnIr4zj19XXMZrnqwsg33s93HyBsDnAhH60RbQ1fgU1ISj386MwlZNp2U7ojAP', '2018-12-15 16:13:18.000000', '2018-12-16 16:16:09.000000');
 
 SET FOREIGN_KEY_CHECKS = 1;
