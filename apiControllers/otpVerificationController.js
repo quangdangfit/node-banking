24d8797a48d44e78448c67a8423749367e92a280
QuangPhuocDang <quang.dp@teko.vn>
2019-01-06 15:14:55 +0700

    update transfer money controller

--- apiControllers/otpVerificationController.js
+++ apiControllers/otpVerificationController.js
@@ -3,49 +3,62 @@ var otpRepo = require('../repos/otpVerificationRepo');
 
 var router = express.Router();
 var authenticator = otpRepo.authenticator;
+var transferRepo = require('../repos/transferRepo');
 
 router.post('/', (req, res) => {
-  var email = req.token_payload.user.email;
-  var user_display_name = req.token_payload.user.first_name + req.token_payload.user.last_name;
+  var input = req.body;
+  input.state = 'to confirm';
 
-  var transaction_token = otpRepo.generateTransactionToken(req.body);
+  transferRepo.add(input)
+    .then(row => {
+      if (row) {
+        var email = req.token_payload.user.email;
+        var user_display_name = req.token_payload.user.first_name + req.token_payload.user.last_name;
 
-  if (!transaction_token) {
-    console.log(error);
+        var transaction_token = otpRepo.generateTransactionToken(row[0]);
 
-    res.statusCode = 404;
-    res.end('Access Token not found');
-  }
+        if (!transaction_token) {
+          console.log(error);
 
-  const otp = authenticator.generate(transaction_token);
+          res.statusCode = 404;
+          res.end('Access Token not found');
+        }
 
-  var mailOptions = {
-    from: process.env.BANK_EMAIL,
-    to: email,
-    subject: 'Internet Banking Verification Email',
-    html: `<p>Dear ${user_display_name},</p>
-    <h2>${otp}</h2>
-    <p>This code will expire 10 minutes after this email was sent</p>
-    <strong>Why you received this email</strong>
-    <p>Internet Banking service require you confirm your transaction.</p>
-    `
-  };
+        const otp = authenticator.generate(transaction_token);
 
-  otpRepo.transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-      console.log(error);
+        var mailOptions = {
+          from: process.env.BANK_EMAIL,
+          to: email,
+          subject: 'Internet Banking Verification Email',
+          html: `<p>Dear ${user_display_name},</p>
+          <h2>${otp}</h2>
+          <p>This code will expire 10 minutes after this email was sent</p>
+          <strong>Why you received this email</strong>
+          <p>Internet Banking service require you confirm your transaction.</p>`
+        };
 
-      res.statusCode = 404;
-      res.end('Not Found');
-    } else {
-      console.log('Email sent: ' + info.response);
+        otpRepo.transporter.sendMail(mailOptions, function (error, info) {
+          if (error) {
+            console.log(error);
 
-      res.json({
-        msg: 'Sent verification email!',
-        transaction_token: transaction_token
-      })
-    }
-  });
+            res.statusCode = 404;
+            res.end('Not Found');
+          } else {
+            console.log('Email sent: ' + info.response);
+
+            res.json({
+              msg: 'Sent verification email!',
+              transaction_token: transaction_token
+            })
+          }
+        });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.statusCode = 500;
+      res.end('View error log on console');
+    });
 });
 
 module.exports = router;
--- apiControllers/transferController.js
+++ apiControllers/transferController.js
@@ -40,73 +40,81 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', otpRepo.verifyTransactionToken, (req, res) => {
-  var input = req.transaction_payload;
+  var transfer_id = req.transaction_payload;
   var min_amount = parseInt(process.env.MIN_AMOUNT) || 50000;
 
-  if (!(input.src_account && input.dest_account && input.amount > min_amount)
-    || (input.src_account === input.dest_account)) {
-    res.json({
-      msg: 'Input is invalid!'
-    })
-  } else {
-    if (!input.fee_type) {
-      input.fee_type = 1
-    }
-    accountRepo.singleByAccNumber(input.src_account)
-      .then((src_account) => {
-          if (src_account) {
-            if (src_account.uid !== req.token_payload.user.uid) {
-              res.json({
-                msg: 'Not allowed!'
-              })
-            } else {
-              var src_balance = parseInt(src_account.balance) - parseInt(input.amount);
-              if (parseInt(input.fee_type) === 2)
-                src_balance -= parseInt(process.env.TRANSFER_FEE);
-              if (src_balance > 0) {
-                accountRepo.updateBalance(src_account.id, src_balance).then((row) => {
-                  accountRepo.singleByAccNumber(input.dest_account).then(dest_account => {
-                    if (dest_account) {
-                      var dest_balance = parseInt(dest_account.balance) + parseInt(input.amount);
-                      if (parseInt(input.fee_type) === 1)
-                        dest_balance -= parseInt(process.env.TRANSFER_FEE);
-                      accountRepo.updateBalance(dest_account.id, dest_balance).then((row) => {
-                        transferRepo.add(input)
-                          .then(row => {
-                            res.json({
-                              msg: 'Transfer is done!'
-                            })
-                          })
-                          .catch(err => {
-                            console.log(err);
-                            res.statusCode = 500;
-                            res.end('View error log on console');
-                          });
-                      });
-                    } else {
-                      res.statusCode = 404;
-                      res.end('Destination Account Not Found');
-                    }
-                  });
-                })
-              } else {
-                res.json({
-                  msg: 'Balance is not enough!'
-                })
-              }
-            }
-          } else {
-            res.statusCode = 404;
-            res.end('Source Account Not Found');
+  transferRepo.single(transfer_id)
+    .then(transfer => {
+      if (transfer) {
+        var input = transfer;
+
+        if (!(input.src_account && input.dest_account && input.amount > min_amount)
+          || (input.src_account === input.dest_account)) {
+          res.json({
+            msg: 'Input is invalid!'
+          })
+        } else {
+          if (!input.fee_type) {
+            input.fee_type = 1
           }
+          accountRepo.singleByAccNumber(input.src_account)
+            .then((src_account) => {
+                if (src_account) {
+                  if (src_account.uid !== req.token_payload.user.uid) {
+                    res.json({
+                      msg: 'Not allowed!'
+                    })
+                  } else {
+                    var src_balance = parseInt(src_account.balance) - parseInt(input.amount);
+                    if (parseInt(input.fee_type) === 2)
+                      src_balance -= parseInt(process.env.TRANSFER_FEE);
+                    if (src_balance > 0) {
+                      accountRepo.updateBalance(src_account.id, src_balance).then((row) => {
+                        accountRepo.singleByAccNumber(input.dest_account).then(dest_account => {
+                          if (dest_account) {
+                            var dest_balance = parseInt(dest_account.balance) + parseInt(input.amount);
+                            if (parseInt(input.fee_type) === 1)
+                              dest_balance -= parseInt(process.env.TRANSFER_FEE);
+                            accountRepo.updateBalance(dest_account.id, dest_balance).then((row) => {
+                              res.json({
+                                msg: 'Transfer is done!'
+                              })
+                            });
+                          } else {
+                            res.statusCode = 404;
+                            res.end('Destination Account Not Found');
+                          }
+                        });
+                      })
+                    } else {
+                      res.json({
+                        msg: 'Balance is not enough!'
+                      })
+                    }
+                  }
+                } else {
+                  res.statusCode = 404;
+                  res.end('Source Account Not Found');
+                }
+              }
+            )
+            .catch(err => {
+              console.log(err);
+              res.statusCode = 500;
+              res.end('View error log on console');
+            });
         }
-      )
-      .catch(err => {
-        console.log(err);
-        res.statusCode = 500;
-        res.end('View error log on console');
-      });
-  }
+      } else {
+        res.json({
+          msg: 'Not allowed!'
+        })
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.statusCode = 500;
+      res.end('View error log on console');
+    });
 });
 
 module.exports = router;
