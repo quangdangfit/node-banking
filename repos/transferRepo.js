e6e2fd4379585759e44b16e4e10b9f6d0cc33f1d
Quang Dang <quangdangfit@gmail.com>
2019-01-06 18:05:00 +0700

    get transaction history in 30 days

--- repos/transferRepo.js
+++ repos/transferRepo.js
@@ -1,7 +1,13 @@
 var kn = require('../fn/db');
 var moment = require('moment');
 
-exports.list = src_account => kn('transfers').select('*').where({'src_account': src_account, 'state': 'done'});
+exports.list = src_account => {
+  var create_at = moment().subtract(30, 'days').format(process.env.DATETIME_FORMAT);
+  return kn('transfers')
+    .select('*')
+    .where({'src_account': src_account, 'state': 'done'})
+    .andWhere('create_at', '>', create_at);
+};
 
 exports.single = id => kn('transfers')
   .select('*')
