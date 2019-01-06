56d2dab41f96327429b17ef01fbc24feccec272a
Quang Dang <quangdangfit@gmail.com>
2019-01-06 15:14:55 +0700

    update transfer money controller

--- repos/transferRepo.js
+++ repos/transferRepo.js
@@ -1,16 +1,16 @@
 var kn = require('../fn/db');
 var moment = require('moment');
 
-exports.list = src_account => kn('transfers').select('*').where('src_account', src_account);
+exports.list = src_account => kn('transfers').select('*').where({'src_account': src_account, 'state': 'done'});
 
 exports.single = id => kn('transfers')
   .select('*')
-  .where('id', id)
+  .where({'id': id})
   .first();
 
 exports.add = input => {
-  input.date = moment().format(process.env.DATETIME_FORMAT);
+  input.create_at = moment().format(process.env.DATETIME_FORMAT);
   return kn('transfers')
     .insert(input)
-    .returning('id');
+    .returning(['id']);
 };
